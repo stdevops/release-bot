08743a131b8aba8c1a61880f66e9c117c7265951
Kevin Gilpin <kgilpin@gmail.com>
2014-11-26 16:25:09 -0500

    Change developer-www-conjur release branch to master

--- lib/sqs/job/message/heroku_release.rb
+++ lib/sqs/job/message/heroku_release.rb
@@ -1,7 +1,7 @@
 module SQS::Job::Message
   class HerokuRelease < Base
     HEROKU_REPOS = [
-      Command::Heroku::App.new("developer-www-conjur", "git@github.com:conjurinc/developer-www.git", "rails", "deploy.sh"),
+      Command::Heroku::App.new("developer-www-conjur", "git@github.com:conjurinc/developer-www.git", "master", "deploy.sh"),
       Command::Heroku::App.new("trialfactory-conjur", "git@github.com:conjurinc/trial-factory.git")
     ].inject({}){|memo,app| memo[app.name] = app; memo}
     
